b0003c461c273a08010ed09234b19ed1345f5a26
Marco Vito Moscaritolo <mavimo@gmail.com>
2018-07-03 23:07:26 +0200

    refs #2: indicate homebrew dependencies

--- git-helper-gh.rb
+++ git-helper-gh.rb
@@ -2,8 +2,12 @@ class GitHelperGh < Formula
   desc "Git helper for GitHub"
   homepage "https://github.com/mavimo/git-helper-gh"
   url "https://github.com/mavimo/git-helper-gh/archive/master.zip"
-  version "0.0.1"
+  version "0.0.2"
   # sha256 ""
+
+  depends_on "jq"
+  depends_on "git"
+  depends_on "gnu-sed"
 
   def install
     bin.install "bin/git-gh-start"
