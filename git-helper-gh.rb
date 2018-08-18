9a87174772f632bec9b1707f8f3c25e3c93ac120
Marco Vito Moscaritolo <mavimo@gmail.com>
2018-08-18 15:03:18 +0200

    added gh-release command

--- git-helper-gh.rb
+++ git-helper-gh.rb
@@ -2,16 +2,17 @@ class GitHelperGh < Formula
   desc "Git helper for GitHub"
   homepage "https://github.com/mavimo/git-helper-gh"
   url "https://github.com/mavimo/git-helper-gh/archive/master.zip"
-  version "0.0.2"
+  version "0.0.3"
   # sha256 ""
 
-  depends_on "jq"
   depends_on "git"
   depends_on "gnu-sed"
+  depends_on "jq"
 
   def install
-    bin.install "bin/git-gh-start"
     bin.install "bin/git-gh-pr"
+    bin.install "bin/git-gh-release"
+    bin.install "bin/git-gh-start"
   end
 
   test do
